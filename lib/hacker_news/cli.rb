427f52eb35e17ac028fc8da6ac314c6991786322
anihakutin <4hbrody@gmail.com>
2018-10-14 12:51:40 -0400

    Menu functionality progress. Option 2, Top articles.

--- lib/hacker_news/cli.rb
+++ lib/hacker_news/cli.rb
@@ -2,18 +2,15 @@
 class HackerNews::CLI
 
   def call
-    display_news
     menu
     good_bye
   end
 
-  def display_news #(args)
-    puts "Welocme to hacker news! \n"
-    puts "----Latest Articles----"
+  def display_news(articles = HackerNews::SCRAPER.new.latest_articles(10))
+    puts "----What you ask is what you get...----"
 
     # request 10 latest_articles
-    new_articles = HackerNews::SCRAPER.new.latest_articles(10)
-    HackerNews::ARTICLE.create_from_collection(new_articles)
+    HackerNews::ARTICLE.create_from_collection(articles)
 
     # Print article objects
     HackerNews::ARTICLE.all.each do |x|
@@ -25,16 +22,20 @@ class HackerNews::CLI
   end
 
   def menu
+    puts "Welocme to hacker news!"
+    puts "Enter the number of the menu option you'd like to see, or type exit to exit:"
     input = nil
-    puts " 1. Show recent news \n 2. Show article with most comments \n 3. Search for article \n 4. Sort articles by source \n 5. List comments \n Please enter a menu option:"
+    puts " 1. Show ten mosts recent posts \n 2. Show ten top posts \n 3. Search for article \n 4. Sort articles by source \n 5. List comments \n "
 
-    input = gets.chomp
+    input = gets.strip.downcase
     if input != "exit"
       case input
       when "1"
-
+        articles = HackerNews::SCRAPER.new.latest_articles(10)
+        display_news(articles)
       when "2"
-
+        articles = HackerNews::SCRAPER.new.top_articles(10)
+        display_news(articles)
       when "3"
 
       when "4"
--- lib/hacker_news/scraper.rb
+++ lib/hacker_news/scraper.rb
@@ -7,7 +7,7 @@ class HackerNews::SCRAPER
     require 'pry'
 
     def latest_articles(count = 10)
-      print "Loading stories."
+      print "Loading articles."
       stories = [ ]
       url = "https://hacker-news.firebaseio.com/v0/newstories.json"
       uri = URI(url)
@@ -38,7 +38,34 @@ class HackerNews::SCRAPER
     end
 
     def top_articles
+      print "Loading articles."
+      stories = [ ]
+      url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+      uri = URI(url)
+      print "."
+      response = Net::HTTP.get(uri)
+      print "."
+      story_ids = JSON.parse(response)
+      print "."
+      i = 0
+      while i < count
+        print "."
+        story_url = "https://hacker-news.firebaseio.com/v0/item/#{story_ids[i]}.json"
+        story_uri = URI(story_url)
+        story_response = Net::HTTP.get(story_uri)
+        story = JSON.parse(story_response)
 
+        new_story = {:title => story["title"],
+                  :type => story["type"],
+                  :author => story["by"],
+                  :time => story["time"],
+                  :text => story["story"],
+                  :url => story["url"]}
+        stories << new_story
+        i += 1
+      end
+      print "\n"
+      stories
     end
 
     def show_articles
