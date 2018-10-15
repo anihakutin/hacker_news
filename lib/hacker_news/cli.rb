58cf7520cc2481bd6e63aac4ee51ad059e371c30
anihakutin <4hbrody@gmail.com>
2018-10-14 20:13:16 -0400

    Formatted some text.

--- lib/hacker_news/cli.rb
+++ lib/hacker_news/cli.rb
@@ -22,35 +22,45 @@ class HackerNews::CLI
   end
 
   def menu
+    input = nil
+
     puts "Welocme to hacker news!"
     puts "Enter the number of the menu option you'd like to see, or type exit to exit:"
-    input = nil
     puts " 1. Show ten mosts recent posts \n 2. Show ten top posts \n 3. Search for article(in top 200) \n 4. Sort articles by source \n 5. List comments \n "
 
     input = gets.strip.downcase
+    
     if input != "exit"
       case input
+
       when "1"
         articles = HackerNews::SCRAPER.new.latest_articles(10)
         display_news(articles)
+
       when "2"
         articles = HackerNews::SCRAPER.new.top_articles(10)
         display_news(articles)
+
       when "3"
         puts "Please select what section you'd like to search \n 1. News \n 2. Top Posts \n 3. Show HN \n 4. Jobs"
         sec_input = gets.strip.downcase
+
         case sec_input
           when "1"
             puts "Please enter your keyword"
             third_input = sec_input = gets.strip.downcase
+
             articles = HackerNews::SCRAPER.new.top_articles(50)
             HackerNews::ARTICLE.create_from_collection(articles)
+
             articles = HackerNews::ARTICLE.find_by_value("story", third_input)
+
              if articles != false
                  display_news(articles)
                else
                  puts "No matching keywords found"
              end
+
           when "2"
 
           when "3"
--- lib/hacker_news/scraper.rb
+++ lib/hacker_news/scraper.rb
@@ -1,26 +1,31 @@
 class HackerNews::SCRAPER
-    # Create method that returns hash with the amount of stories requested
-    # Create method that returns the top 500 topstories, beststories or newstories
-    # Create method that returns stories comments
+
     require 'net/http'
     require 'json'
     require 'pry'
 
     def latest_articles(count = 10)
-      print "Loading articles."
       stories = [ ]
+      
+      print "Loading articles."
+
       url = "https://hacker-news.firebaseio.com/v0/newstories.json"
       uri = URI(url)
       print "."
+
       response = Net::HTTP.get(uri)
       print "."
+
       story_ids = JSON.parse(response)
       print "."
+
       i = 0
       while i < count
         print "."
+
         story_url = "https://hacker-news.firebaseio.com/v0/item/#{story_ids[i]}.json"
         story_uri = URI(story_url)
+
         story_response = Net::HTTP.get(story_uri)
         story = JSON.parse(story_response)
 
@@ -33,25 +38,33 @@ class HackerNews::SCRAPER
         stories << new_story
         i += 1
       end
+
       print "\n"
       stories
     end
 
     def top_articles(count = 10)
-      print "Loading articles."
       stories = [ ]
+
+      print "Loading articles."
+
       url = "https://hacker-news.firebaseio.com/v0/topstories.json"
       uri = URI(url)
       print "."
+
       response = Net::HTTP.get(uri)
       print "."
+
       story_ids = JSON.parse(response)
       print "."
+
       i = 0
       while i < count
         print "."
+
         story_url = "https://hacker-news.firebaseio.com/v0/item/#{story_ids[i]}.json"
         story_uri = URI(story_url)
+
         story_response = Net::HTTP.get(story_uri)
         story = JSON.parse(story_response)
 
@@ -61,9 +74,11 @@ class HackerNews::SCRAPER
                   :time => story["time"],
                   :text => story["story"],
                   :url => story["url"]}
+
         stories << new_story
         i += 1
       end
+
       print "\n"
       stories
     end
